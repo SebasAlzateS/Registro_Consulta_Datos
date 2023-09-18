3d3c3289ebcb6e973da5ab24d593d7dc4cdd1685
Sebastián Alzate Sierra <141065878+SebasAlzateS@users.noreply.github.com>
2023-09-18 17:59:14 -0500

    Add files via upload

---
+++ Registro_consulta_Estudiantes.py
@@ -0,0 +1,13 @@
+'''
+Programa principal
+Registro y Consulta de estudiantes Universitarios 
+
+Sebastián Alzate Sierra
+Maria Paula Ramos Martínez
+xxxxxxxxxxxxxxxx
+
+Taller #2 Programación
+'''
+import functions as ft
+ft.load_data()  #Cargar base de datos si existe
+ft.menu()       #Ejecuta el menú
