0c01fc73c07c541d9dddbd1b8ebacc008a597df3
MariaRamos04 <143203403+MariaRamos04@users.noreply.github.com>
2023-09-18 22:20:56 -0500

    Update Registro_consulta_Estudiantes.py

--- Registro_consulta_Estudiantes.py
+++ Registro_consulta_Estudiantes.py
@@ -9,5 +9,13 @@ Juan Isaza
 Taller #2 Programación
 '''
 import functions as ft
-ft.load_data()  #Cargar base de datos si existe
-ft.menu()       #Ejecuta el menú
+
+def run():
+    ft.load_data()  # Cargar base de datos si existe
+    ft.menu()       # Ejecutar el menú
+
+#Entry Point
+if __name__ == "__main__": # Determina si el archivo actual se está ejecutando como programa principal.
+    print("\nCÓDIGO PRINCIPAL\n ") # Imprimir un mensaje indicando que se está ejecutando el código principal.
+    run()  # Llamar a la función 'run' para iniciar la ejecución del programa.
+    
